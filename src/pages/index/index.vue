ad0f451974450a75a822dae58031efb2dae37b1a
dongjc <2806430618@qq.com>
2019-10-30 00:03:01 +0800

    index modified

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,9 +5,13 @@
         <bannerSwiper :imgUrls="imgUrls"></bannerSwiper>
       </view>
       <view style="margin-top:10rpx">
-        <van-tabs :active="tabActive" swipeable animated line-height="0px" @change="onChange">
-          <van-tab title="精选">内容 1</van-tab>
-          <van-tab title="百货">内容 2</van-tab>
+        <van-tabs sticky :active="tabActive" swipeable animated line-height="0px" @change="onChange">
+          <van-tab title="精选">
+            <view v-for="item in 100" :key ="item">{{item}}</view>
+          </van-tab>
+          <van-tab title="百货">
+            <view v-for="item in 100" :key ="item">{{item}}</view>
+          </van-tab>
           <van-tab title="食品">内容 3</van-tab>
           <van-tab title="内衣">内容 4</van-tab>
           <van-tab title="水果">内容 4</van-tab>
@@ -32,6 +36,19 @@ export default {
         'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572167835009&di=9a7b805520cad4b7bdbc49bb8a45066d&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F037094356e1859e32f875520f774466.jpg'
       ]
     }
+  },
+  onReachBottom() {
+  // 到这底部在这里需要做什么事情
+    console.log(123)
+  },
+  onPullDownRefresh() {
+    console.log(123)
+    wx.showNavigationBarLoading()
+    setTimeout(function() {
+      // complete
+      wx.hideNavigationBarLoading() // 完成停止加载
+      wx.stopPullDownRefresh() // 停止下拉刷新
+    }, 1500)
   },
   methods: {
     bindViewTap() {
