b898f3685930c1bcef209b31b4ae79a30ac9a855
dongjc <2806430618@qq.com>
2019-11-05 23:29:54 +0800

    modify search add history

--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -5,8 +5,8 @@
             <view slot="action" @tap="onSearch" style="color:white">搜索</view>
           </van-search>
         </view>
-        <view class="search-container">
-            <view style="display:flex;justify-content:space-between;padding:18rpx;position:sticky;top:54px;background:#fff;font-size:27rpx;color:#585858">
+        <view v-if="searchListItem.length !== 0" class="search-container">
+            <view style="display:flex;justify-content:space-between;padding:18rpx;position:sticky;top:54px;font-size:27rpx;color:#585858;background:#fff">
               <!-- 综合  销量  规格  筛选-->
               <view>综合</view>
               <view>销量</view>
@@ -14,7 +14,32 @@
               <view>筛选</view>
             </view>
             <indexList v-for="item in 10" :key="item"></indexList>
+            <!-- 最近搜索 -->
         </view>
+        <view v-else class="history-container" style="margin-top:60px">
+          <view class="history-container-title">
+              <text class="iconfont share" style="font-size:28rpx"></text>
+              <text>最近搜索</text>
+              <text class="iconfont share"></text>
+          </view>
+          <view class="history-container-item">
+            <view>可惜不是你嗷嗷</view>
+            <view>22221</view>
+            <view>一生衣服</view>
+            <view>22221</view>
+          </view>
+          <view class="history-container-title">
+              <text class="iconfont share" style="font-size:28rpx"></text>
+              <text>探索发现</text>
+          </view>
+          <view class="history-container-item">
+            <view>衣服扣子</view>
+            <view>电话充费</view>
+            <view>联通大王卡</view>
+            <view>办公电脑</view>
+          </view>
+        </view>
+        <!-- 搜索发现 -->
     </view>
 </template>
 <script>
@@ -25,11 +50,20 @@ export default {
   components: { indexList },
   data() {
     return {
-      searchValue: ''
+      searchValue: '',
+      searchListItem: []
     }
   },
   onLoad(param) {
+    this.searchListItem = []
     console.log(param)
+  },
+  methods: {
+    onSearch() {
+      console.log(123)
+      this.searchListItem.push(1)
+      console.log(this.searchListItem)
+    }
   }
 }
 </script>
@@ -41,10 +75,38 @@ page{
   padding: 10px 0 10px 16px!important;
 }
 .search-container{
-  margin-top:54px;
-  background:#fff;
+  margin-top:50px;
   .item-container{
     border-bottom: 5rpx solid #eee;
+  }
+}
+.history-container{
+  margin: 0 25rpx;
+  &-title {
+  display:flex;
+  align-items:center;
+  color:gray;
+  font-size:26rpx;
+    :nth-child(2){
+      margin-left: 4rpx
+    }
+    :nth-child(3){
+      margin-left:auto;
+      font-size:28rpx;
+    }
+  }
+  &-item{
+    display:flex;
+    flex-wrap:wrap;
+    margin:10rpx;
+    font-size:25rpx;
+    view{
+      margin:0 13rpx 13rpx 0;
+      padding: 13rpx;
+      background:#eee;
+      color:#363636;
+      border-radius:30rpx
+    }
   }
 }
 </style>
