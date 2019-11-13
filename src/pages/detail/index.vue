88b9215bdd6031cedb04df009dd56559e1efd32c
dongjc <2806430618@qq.com>
2019-11-13 23:20:33 +0800

    modify details swiper/css and search add iconfont

--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -1,19 +1,22 @@
 <template>
   <view class="detail-container">
-    <text class="iconfont iconfanhui back" @tap="back"></text>
+    <view class="back" @tap="back">
+      <image src="/static/images/back.png" />
+    </view>
     <!-- 预览图 -->
-    <swiper :indicator-dots="false" style="height:360px;">
+    <swiper :indicator-dots="false" :style="{height: swiperHeight}">
       <block>
         <swiper-item>
           <image
+            @load="imageHeight"
+            :style="{height: swiperHeight,width:'100%'}"
             src="https://img.alicdn.com/imgextra/i1/356060330/O1CN01jV2NDF1EJBGpElxQi_!!356060330.jpg_430x430q90.jpg"
-            style="width:100%;height:360px"
           />
         </swiper-item>
         <swiper-item>
           <image
+            :style="{height: swiperHeight,width:'100%'}"
             src="https://img.alicdn.com/imgextra/i1/356060330/O1CN01jV2NDF1EJBGpElxQi_!!356060330.jpg_430x430q90.jpg"
-            style="width:100%;height:360px"
           />
         </swiper-item>
       </block>
@@ -27,7 +30,7 @@
           <text>17.89</text>
           <text class="font-27">¥19.89</text>
         </view>
-        <view class="commodity-assemble">已拼10万+件</view>
+        <view class="commodity-assemble font-25">已拼10万+件</view>
       </view>
       <view class="commodity-coupon">
         <text>发券</text>
@@ -42,7 +45,7 @@
         <text class="iconfont iconyou right" style="margin-left:auto"></text>
       </view>
       <text class="commodity-name">富贵秋冬女加绒羽绒服上衣粉红色少女长低领针毛绒</text>
-      <view class="commodity-service">
+      <view class="commodity-service font-25">
         <text>退货包运费 · 极速退款 · 极速退款 · 极速退款</text>
         <text class="iconfont iconyou" style="margin-left:auto"></text>
       </view>
@@ -50,7 +53,7 @@
     <view class="similar-commodity">
       <view class="similar-title">
         <view>相似商品</view>
-        <view style="color:#91918f;display:flex;align-items:center">
+        <view class="all-similar">
           <text>查看全部</text>
           <text class="iconfont iconyou"></text>
         </view>
@@ -73,116 +76,89 @@
     <view class="similar-commodity">
       <view class="similar-title">
         <view>商品评价</view>
-        <view style="color:#91918f;display:flex;align-items:center">
+        <view class="right-all">
           <text>查看全部</text>
           <text class="iconfont iconyou"></text>
         </view>
       </view>
       <!-- 评论 -->
-      <view class="more-commodity" style="display:flex;flex-direction:column">
+      <view class="more-commodity comment-container">
         <!-- 评论标签 -->
-        <view style="font-size:24rpx;display:flex;margin-bottom:10rpx">
-          <view
-            style="padding:10rpx;background:#f6e8eb;border-radius:10rpx;color:#333;margin-right:12rpx"
-          >正品(18)</view>
-          <view
-            style="padding:10rpx;background:#f6e8eb;border-radius:10rpx;color:#333;margin-right:12rpx"
-          >质量很好(10218)</view>
-          <view
-            style="padding:10rpx;background:#f6e8eb;border-radius:10rpx;color:#333;margin-right:12rpx"
-          >衣服很好(389218)</view>
+        <view class="comment-tag">
+          <view class="comment-tag-item">正品(18)</view>
+          <view class="comment-tag-item">质量很好(10218)</view>
+          <view class="comment-tag-item">衣服很好(389218)</view>
         </view>
         <!-- 评论列表 -->
-        <view
-          style="display:flex;flex-direction:column;font-size:24rpx;padding:15rpx 0;border-bottom:2rpx solid #eee"
-        >
-          <view
-            style="display:flex;justify-content:space-between;align-items:center;margin-bottom:10rpx"
-          >
-            <view style="display:flex;align-items:center;">
+        <view class="comment-list">
+          <view class="comment-list-top">
+            <view class="comment-list-top-left">
               <image
                 src="https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1573316656&di=e6346dfae80b451c19ccf8e6253ae28a&src=http://downhdlogo.yy.com/hdlogo/640640/630/630/71/1000710067/u1000710067JGb2l8C.png"
-                style="width:40rpx;height:40rpx;border-radius:50%;margin-right:10rpx"
               />
               <text>凌波的云</text>
             </view>
-            <view style="color:#91918f">2019.10.10</view>
+            <view class="color-91918f">2019.10.10</view>
           </view>
           <view style="color:#696969">这件衣服很好看。。。</view>
         </view>
       </view>
     </view>
-    <!-- 分享以及立刻购买 -->
-    <view
-      style="position:fixed;display:flex;height:100rpx;background:red;bottom:0;width:100%;font-size:25rpx;color:white"
-    >
-      <view
-        style="background:#eaa59e;flex:1;display:flex;align-items:center;justify-content:center;flex-direction:column"
-      >
-        <view style="font-size:31rpx">分享商品</view>
-        <view>
-          <text class="iconfont iconjinbi" style="font-size:25rpx"></text>拿奖励
-        </view>
-      </view>
-      <view
-        style="background:#d44f38;flex:1;display:flex;align-items:center;justify-content:center;flex-direction:column"
-      >
-        <view style="font-size:31rpx">立即购买</view>
-        <view>省3.26元</view>
-      </view>
-    </view>
     <!-- 店铺 -->
     <view class="similar-commodity">
-      <view
-        style="display:flex;justify-content:space-between;align-items:center;padding:15rpx 0;margin-bottom:15rpx"
-      >
-        <view style="display:flex;align-items:center">
+      <view class="commodity-shop">
+        <view class="commodity-shop-top">
           <image
             src="https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1573316656&di=e6346dfae80b451c19ccf8e6253ae28a&src=http://downhdlogo.yy.com/hdlogo/640640/630/630/71/1000710067/u1000710067JGb2l8C.png"
-            style="width:70rpx;height:70rpx;border-radius:10rpx;margin-right:10rpx"
           />
-          <view style="display:flex;flex-direction:column;font-size:24rpx;margin-left:10rpx">
+          <view class="commodity-shop-top-left">
             <view>
               <text style="font-size:27rpx">良品店子铺</text>
-              <text
-                style="padding:2rpx;background:#f7e3e5;border-radius:8rpx;font-size:21rpx;margin-left:10rpx;color:	#FF6347"
-              >全国推广1%</text>
+              <text class="commodity-shop-top-left-extension">全国推广1%</text>
             </view>
-            <view style="margin-top:8rpx;font-size:21rpx;color:#91918f">
+            <view class="commodity-shop-top-left-number">
               <text>商品数量：9</text>
               <text style="margin-left:18rpx">已拼：10万+件</text>
             </view>
           </view>
         </view>
-        <view style="padding:6rpx;border-radius:10rpx;font-size:24rpx;border:2rpx solid #eee">进入店铺</view>
+        <view class="commodity-shop-bottom">进入店铺</view>
       </view>
-      <view
-        style="display:flex;justify-content:space-between;font-size:24rpx;padding:0 40rpx 10rpx"
-      >
+      <view class="shop-comment">
         <text>
           描述相符
-          <text style="background:#f7e3e5;color:#FF6347;padding:0 6rpx;">高</text>
+          <text class="shop-comment-status">高</text>
         </text>
         <text>
           服务态度
-          <text style="background:#f7e3e5;color:#FF6347;padding:0 6rpx;">高</text>
+          <text class="shop-comment-status">高</text>
         </text>
         <text>
           物流服务
-          <text style="background:#f7e3e5;color:#FF6347;padding:0 6rpx;">高</text>
+          <text class="shop-comment-status">高</text>
         </text>
       </view>
     </view>
     <!-- 详情 -->
     <view class="similar-commodity">
-      <view class="similar-title">
-        <view>商品详情</view>
-      </view>
-      <view style="margin-top:15rpx;padding-bottom:100rpx">
+      <view class="similar-title">商品详情</view>
+      <view class="commodity-details">
         <image
           src="https://img.alicdn.com/imgextra/i1/356060330/O1CN01jV2NDF1EJBGpElxQi_!!356060330.jpg_430x430q90.jpg"
-          style="width:100%;height:600rpx"
         />
+      </view>
+    </view>
+    <!-- 分享以及立刻购买 -->
+    <view class="share-container font-25">
+      <view class="share-purchase-commodity bg-left">
+        <view class="font-31">分享商品</view>
+        <view>
+          <text class="iconfont iconjinbi font-25"></text>拿奖励
+        </view>
+      </view>
+      <view class="share-purchase-commodity bg-right">
+        <view class="font-31">立即购买</view>
+        <view>省3.26元</view>
       </view>
     </view>
   </view>
@@ -191,11 +167,20 @@
 export default {
   name: 'detail',
   data() {
-    return {}
+    return {
+      swiperHeight: '430px'
+    }
   },
   methods: {
     back() {
       wx.navigateBack()
+    },
+    imageHeight(e) {
+      // https://www.jb51.net/article/138776.htm 计算swiper图片
+      const winWid = wx.getSystemInfoSync().windowWidth
+      const imagw = e.target.width
+      const imagh = e.target.height
+      this.swiperHeight = (winWid * imagh / imagw) + 'px'
     }
   }
 }
@@ -204,29 +189,39 @@ export default {
 page {
   background: #f6f5f4;
 }
+.font-25 {
+  font-size: 25rpx;
+}
 .font-27 {
   font-size: 27rpx;
+}
+.font-31 {
+  font-size: 31rpx;
 }
 .iconyou {
   font-size: 21rpx;
   margin-left: 3rpx;
 }
 .iconjinbi {
-  font-size: 25rpx;
   color: yellow;
   margin-right: 6rpx;
+}
+.color-91918f {
+  color: #91918f;
 }
 .detail-container {
   .back {
     font-size: 45rpx;
     position: fixed;
-    top: 33px;
+    top: 28px;
     left: 27px;
     z-index: 1;
-    background: #808281;
     border-radius: 50%;
-    box-shadow: 10rpx 10rpx 10 rpx red;
     color: #fff;
+    image {
+      width: 27px;
+      height: 27px;
+    }
   }
   .commodity-container {
     padding: 10rpx 27rpx 0 27rpx;
@@ -252,7 +247,6 @@ page {
         }
       }
       .commodity-assemble {
-        font-size: 25rpx;
         color: #808281;
       }
     }
@@ -302,7 +296,6 @@ page {
       display: flex;
       padding: 18rpx 0;
       align-items: center;
-      font-size: 25rpx;
       color: #91918f;
     }
   }
@@ -316,6 +309,116 @@ page {
       justify-content: space-between;
       border-bottom: 2rpx solid #eee;
       font-size: 27rpx;
+      /** 查看全部按钮 */
+      .right-all {
+        display: flex;
+        align-items: center;
+        color: #91918f;
+      }
+    }
+    /** 评论列表 */
+    .comment-container {
+      display: flex;
+      flex-direction: column;
+      .comment-tag {
+        display: flex;
+        margin-bottom: 10rpx;
+        font-size: 24rpx;
+        &-item {
+          margin-right: 12rpx;
+          padding: 10rpx;
+          background: #f6e8eb;
+          border-radius: 10rpx;
+          color: #333;
+        }
+      }
+      .comment-list {
+        display: flex;
+        flex-direction: column;
+        padding: 15rpx 0;
+        font-size: 24rpx;
+        border-bottom: 2rpx solid #eee;
+        &-top {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          margin-bottom: 10rpx;
+          &-left {
+            display: flex;
+            align-items: center;
+            image {
+              margin-right: 10rpx;
+              width: 40rpx;
+              height: 40rpx;
+              border-radius: 50%;
+            }
+          }
+        }
+      }
+    }
+    /** 商铺 */
+    .commodity-shop {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 15rpx;
+      padding: 15rpx 0;
+      &-top {
+        display: flex;
+        align-items: center;
+        image {
+          margin-right: 10rpx;
+          width: 70rpx;
+          height: 70rpx;
+          border-radius: 10rpx;
+        }
+        &-left {
+          display: flex;
+          flex-direction: column;
+          margin-left: 10rpx;
+          font-size: 24rpx;
+          &-extension {
+            margin-left: 10rpx;
+            padding: 2rpx;
+            background: #f7e3e5;
+            border-radius: 8rpx;
+            font-size: 21rpx;
+            color: #ff6347;
+          }
+          &-number {
+            margin-top: 8rpx;
+            font-size: 21rpx;
+            color: #91918f;
+          }
+        }
+      }
+      &-bottom {
+        padding: 6rpx;
+        border-radius: 10rpx;
+        font-size: 24rpx;
+        border: 2rpx solid #eee;
+      }
+    }
+    /** 商铺评价 */
+    .shop-comment {
+      display: flex;
+      justify-content: space-between;
+      padding: 0 40rpx 10rpx;
+      font-size: 24rpx;
+      &-status {
+        padding: 0 6rpx;
+        background: #f7e3e5;
+        color: #ff6347;
+      }
+    }
+    /** 商品详情 */
+    .commodity-details {
+      margin-top: 15rpx;
+      padding-bottom: 100rpx;
+      image {
+        width: 100%;
+        height: 600rpx;
+      }
     }
     .more-commodity {
       display: flex;
@@ -340,6 +443,28 @@ page {
           color: #be5041;
         }
       }
+    }
+  }
+  .share-container {
+    position: fixed;
+    display: flex;
+    bottom: 0;
+    width: 100%;
+    height: 100rpx;
+    background: red;
+    color: white;
+    .share-purchase-commodity {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      flex: 1;
+    }
+    .bg-left {
+      background: #eaa59e;
+    }
+    .bg-right {
+      background: #d44f38;
     }
   }
 }
--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -1,46 +1,55 @@
 <template>
-    <view>
-        <view style="position:fixed;width:100%;background:#dc6364;display:flex;top:0;height:54px">
-          <van-search style="flex:1" :focus="true" background="null" use-action-slot :value="searchValue" placeholder="请输入搜索的商品">
-            <view slot="action" @tap="onSearch" style="color:white">搜索</view>
-          </van-search>
-        </view>
-        <view v-if="searchListItem.length !== 0" class="search-container">
-            <view style="display:flex;justify-content:space-between;padding:18rpx;position:sticky;top:54px;font-size:27rpx;color:#585858;background:#fff">
-              <!-- 综合  销量  规格  筛选-->
-              <view>综合</view>
-              <view>销量</view>
-              <view>规格</view>
-              <view>筛选</view>
-            </view>
-            <indexList v-for="item in 10" :key="item"></indexList>
-            <!-- 最近搜索 -->
-        </view>
-        <view v-else class="history-container" style="margin-top:60px">
-          <view class="history-container-title">
-              <text class="iconfont share" style="font-size:28rpx"></text>
-              <text>最近搜索</text>
-              <text class="iconfont share"></text>
-          </view>
-          <view class="history-container-item">
-            <view>可惜不是你嗷嗷</view>
-            <view>22221</view>
-            <view>一生衣服</view>
-            <view>22221</view>
-          </view>
-          <view class="history-container-title">
-              <text class="iconfont share" style="font-size:28rpx"></text>
-              <text>探索发现</text>
-          </view>
-          <view class="history-container-item">
-            <view>衣服扣子</view>
-            <view>电话充费</view>
-            <view>联通大王卡</view>
-            <view>办公电脑</view>
-          </view>
-        </view>
-        <!-- 搜索发现 -->
+  <view>
+    <view class="search-input">
+      <van-search
+        style="flex:1"
+        :focus="true"
+        background="null"
+        use-action-slot
+        :value="searchValue"
+        placeholder="请输入搜索的商品"
+      >
+        <view slot="action" @tap="onSearch" style="color:white">搜索</view>
+      </van-search>
     </view>
+    <view v-if="searchListItem.length !== 0" class="search-container">
+      <view
+        style="display:flex;justify-content:space-between;padding:18rpx;position:sticky;top:54px;font-size:27rpx;color:#585858;background:#fff"
+      >
+        <!-- 综合  销量  规格  筛选-->
+        <view>综合</view>
+        <view>销量</view>
+        <view>规格</view>
+        <view>筛选</view>
+      </view>
+      <indexList v-for="item in 10" :key="item"></indexList>
+      <!-- 最近搜索 -->
+    </view>
+    <view v-else class="history-container" style="margin-top:60px">
+      <view class="history-container-title">
+        <text class="iconfont iconjiaoji"></text>
+        <text>最近搜索</text>
+        <text class="iconfont iconshanchu"></text>
+      </view>
+      <view class="history-container-item">
+        <view>可惜不是你嗷嗷</view>
+        <view>22221</view>
+        <view>一生衣服</view>
+        <view>22221</view>
+      </view>
+      <view class="history-container-title">
+        <text class="iconfont icontansuo"></text>
+        <text>探索发现</text>
+      </view>
+      <view class="history-container-item">
+        <view>衣服扣子</view>
+        <view>电话充费</view>
+        <view>联通大王卡</view>
+        <view>办公电脑</view>
+      </view>
+    </view>
+    <!-- 搜索发现 -->
+  </view>
 </template>
 <script>
 import indexList from '@/components/index-list'
@@ -68,44 +77,54 @@ export default {
 }
 </script>
 <style lang="scss">
-page{
-  background:#f6f5f4;
+page {
+  background: #f6f5f4;
 }
-.van-search{
-  padding: 10px 0 10px 16px!important;
+.iconfont {
+  font-size: 38rpx;
 }
-.search-container{
-  margin-top:50px;
-  .item-container{
+.van-search {
+  padding: 10px 0 10px 16px !important;
+}
+.search-input {
+  position: fixed;
+  display: flex;
+  top: 0;
+  width: 100%;
+  height: 54px;
+  background: #dc6364;
+}
+.search-container {
+  margin-top: 50px;
+  .item-container {
     border-bottom: 5rpx solid #eee;
   }
 }
-.history-container{
+.history-container {
   margin: 0 25rpx;
   &-title {
-  display:flex;
-  align-items:center;
-  color:gray;
-  font-size:26rpx;
-    :nth-child(2){
-      margin-left: 4rpx
+    display: flex;
+    align-items: center;
+    color: gray;
+    font-size: 28rpx;
+    :nth-child(2) {
+      margin-left: 4rpx;
     }
-    :nth-child(3){
-      margin-left:auto;
-      font-size:28rpx;
+    :nth-child(3) {
+      margin-left: auto;
     }
   }
-  &-item{
-    display:flex;
-    flex-wrap:wrap;
-    margin:10rpx;
-    font-size:25rpx;
-    view{
-      margin:0 13rpx 13rpx 0;
+  &-item {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 10rpx;
+    font-size: 27rpx;
+    view {
+      margin: 0 13rpx 13rpx 0;
       padding: 13rpx;
-      background:#eee;
-      color:#363636;
-      border-radius:30rpx
+      background: #eee;
+      color: #363636;
+      border-radius: 30rpx;
     }
   }
 }
