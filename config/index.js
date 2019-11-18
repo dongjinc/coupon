7f8ed73cc504f74a04d3705c4be193f19bd4e1d3
yuhao <ityuhao@gmail.com>
2019-11-19 00:34:13 +0800

    增加prod预览.

--- config/index.js
+++ config/index.js
@@ -64,5 +64,21 @@ module.exports = {
     // just be aware of this issue when enabling this option.
     cssSourceMap: false,
     fileExt: fileExt
+  },
+  prod: {
+    env: require('./dev.env'),
+    port: 8080,
+    // 在小程序开发者工具中不需要自动打开浏览器
+    autoOpenBrowser: false,
+    assetsSubDirectory: '',
+    assetsPublicPath: '/',
+    proxyTable: {},
+    // CSS Sourcemaps off by default because relative paths are "buggy"
+    // with this option, according to the CSS-Loader README
+    // (https://github.com/webpack/css-loader#sourcemaps)
+    // In our experience, they generally work as expected,
+    // just be aware of this issue when enabling this option.
+    cssSourceMap: false,
+    fileExt: fileExt
   }
 }
